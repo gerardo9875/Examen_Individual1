f3211f821debc1599001ecc8e17e0e122c59a4ca
Gerardo Barboza <gerardo.barboza@hotmail.com>
2023-10-31 15:34:27 -0500

    First commit and player movement

---
+++ Assets/Scripts/Player Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        rb2d.velocity = new Vector2 (moveX, moveY);
+    }
+
+}
